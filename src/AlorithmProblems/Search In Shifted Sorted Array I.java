f50439bc3efa57fb7c760d6f38aff1f816d02b6e
Jingle <63937628+JingleMeow@users.noreply.github.com>
2021-02-23 09:56:37 -0500

    Use while loop instead of recursion

--- src/AlorithmProblems/Search In Shifted Sorted Array I.java
+++ src/AlorithmProblems/Search In Shifted Sorted Array I.java
@@ -2,26 +2,24 @@ public class Solution {
     public int search(int[] array, int target) {
         if (array == null || array.length == 0)
             return -1;
-        return search(array, 0, array.length - 1, target);
-    }
-
-    public int search(int[] array, int left, int right, int target) {
-        if (left > right)
-            return -1;
-
-        int mid = left + (right - left) / 2;
-        if (target == array[mid])
-            return mid;
-        if (array[mid] < array[left]) {
-            // pivot in left part [left, mid]
-            if (array[mid] < target && array[right] >= target)
-                return search(array, mid + 1, right, target);
-            return search(array, left, mid - 1, target);
-        } else {
-            // pivot in right part [mid + 1, right]
-            if (array[left] <= target && array[mid] > target)
-                return search(array, left, mid - 1, target);
-            return search(array, mid + 1, right, target);
+        int left = 0, right = array.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (array[mid] == target)
+                return mid;
+            
+            if (array[left] <= array[mid]) {
+                if (array[left] <= target && target < array[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            } else {
+                if (array[mid] < target && target <= array[right]) 
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
         }
+        return -1;
     }
 }
